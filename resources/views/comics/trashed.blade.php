c75c2ac946dd5e4d97094dc1fb45411e21f46785
LorisMarzocchi <ml77182a@gmail.com>
2023-07-03 18:33:30 +0200

    AddConfirmationForHardDelete.

--- resources/views/comics/trashed.blade.php
+++ resources/views/comics/trashed.blade.php
@@ -46,14 +46,44 @@
                         <form class=" d-inline-block " action="{{ route('comics.restore', ['comic' => $comic->id]) }}"
                             method="POST">
                             @csrf
-                            <button class="btn btn-success">Ripristina</button>
+                            <button class="btn btn-success">Restore</button>
                         </form>
-                        <form class=" d-inline-block " action="{{ route('comics.hardDelete', ['comic' => $comic->id]) }}"
-                            method="POST">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-success">Hard Delete</button>
-                        </form>
+
+
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Hard Delete
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm Action</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Are you sure you want to proceed with the deletion? This action cannot be undone.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Cancel</button>
+                                        <form class=" d-inline-block "
+                                            action="{{ route('comics.hardDelete', ['comic' => $comic->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="btn btn-success">Hard Delete</button>
+                                            {{-- <button class="btn btn-success"
+                                                onclick="return confirm('Are you sure you want to permanently delete this resource?')">Hard
+                                                Delete</button> --}}
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             @endforeach
